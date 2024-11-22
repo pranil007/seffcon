56e8eaa93b0aa0b258688e01ad4e3006ed671ad7
Pranil <53268924+pranil007@users.noreply.github.com>
2024-11-22 22:29:38 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+
+(function ($) {
+  "use strict";
+
+
+  function thmSwiperInit() {
+    // swiper slider
+    if ($(".thm-swiper__slider").length) {
+      $(".thm-swiper__slider").each(function () {
+        let elm = $(this);
+        let options = elm.data("swiper-options");
+        let thmSwiperSlider = new Swiper(elm, options);
+      });
+    }
+  }
+
+  function thmOwlInit() {
+    // owl slider
+
+    if ($(".thm-owl__carousel").length) {
+      $(".thm-owl__carousel").each(function () {
+        let elm = $(this);
+        let options = elm.data("owl-options");
+        let thmOwlCarousel = elm.owlCarousel(options);
+      });
+    }
+  }
+
+
+
+
+
+  if ($(".banner-bg-slide").length) {
+    $(".banner-bg-slide").each(function () {
+      var Self = $(this);
+      var bgSlideOptions = Self.data("options");
+      var bannerTwoSlides = Self.vegas(bgSlideOptions);
+    });
+  }
+
+
+
+  // Accrodion
+  if ($(".accrodion-grp").length) {
+    var accrodionGrp = $(".accrodion-grp");
+    accrodionGrp.each(function () {
+      var accrodionName = $(this).data("grp-name");
+      var Self = $(this);
+      var accordion = Self.find(".accrodion");
+      Self.addClass(accrodionName);
+      Self.find(".accrodion .accrodion-content").hide();
+      Self.find(".accrodion.active").find(".accrodion-content").show();
+      accordion.each(function () {
+        $(this)
+          .find(".accrodion-title")
+          .on("click", function () {
+            if ($(this).parent().hasClass("active") === false) {
+              $(".accrodion-grp." + accrodionName)
+                .find(".accrodion")
+                .removeClass("active");
+              $(".accrodion-grp." + accrodionName)
+                .find(".accrodion")
+                .find(".accrodion-content")
+                .slideUp();
+              $(this).parent().addClass("active");
+              $(this).parent().find(".accrodion-content").slideDown();
+            }
+          });
+      });
+    });
+  }
+
+
+
+  if ($(".wow").length) {
+    var wow = new WOW({
+      boxClass: "wow",
+      animateClass: "animated",
+      mobile: true,
+      live: true
+    });
+    wow.init();
+  }
+
+
+
+
+  // window load event
+
+  $(window).on("load", function () {
+    thmSwiperInit();
+    thmOwlInit();
+  });
+
+
+
+})(jQuery);
